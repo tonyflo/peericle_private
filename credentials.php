0f536baea3d754656d3600da5856091d1a17abc0
Tony Florida <raddfood@gmail.com>
2014-09-21 17:35:17 -0700

    Initial commit of private repository for Peericle. The purpose of this repository is to hide confidential information from the public-facing website such as usernames and passwords for the peericle database. This repo should be cloned alongside of the public-facing directory for Peericle so that HTML, PHP, etc can refer to the contents relatively.

---
+++ credentials.php
@@ -0,0 +1,19 @@
+<?php
+/* @file credentials.php
+ * @date 2014-09-21
+ * @author Tony Florida
+ * @brief Logic to connect to the peericle database
+ */
+   $user="peericle_u";
+   $password="getup2speed";
+   $database="peericledb";
+   $host="mysql.peericle.org";
+
+   $db = new mysqli($host, $user, $password, $database);
+   if($db->connect_errno > 0)
+   {
+      die('Unable to connect to database [' . $db->connect_error . ']');
+   }
+   error_reporting(E_ERROR);
+?>
+
