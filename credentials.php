ed6baebdd5679d95b69d968fa7a0079d99bcf40c
Nick Mathews <nickinnik@gmail.com>
2014-09-21 19:31:41 -0700

    Using tabs of width 4

--- credentials.php
+++ credentials.php
@@ -4,16 +4,17 @@
  * @author Tony Florida
  * @brief Logic to connect to the peericle database
  */
-   $user="peericle_u";
-   $password="getup2speed";
-   $database="peericledb";
-   $host="mysql.peericle.org";
+	$user="peericle_u";
+	$password="getup2speed";
+	$database="peericledb";
+	$host="mysql.peericle.org";
 
-   $db = new mysqli($host, $user, $password, $database);
-   if($db->connect_errno > 0)
-   {
-      die('Unable to connect to database [' . $db->connect_error . ']');
-   }
-   error_reporting(E_ERROR);
+	$db = new mysqli($host, $user, $password, $database);
+	if($db->connect_errno > 0)
+	{
+		die('Unable to connect to database [' . $db->connect_error . ']');
+	}
+
+	error_reporting(E_ERROR);
 ?>
 
