0593e21e73743a453173f191cc4692be0f06460a
Tony Florida <raddfood@gmail.com>
2014-09-22 19:23:02 -0700

    Added an array (key->value) to the return codes. Only problem is that we're using AJAX which is Javascript-based and we don't have access to this array.  I need to figure out a way to have a single set of status codes that are accessible on both the client and server side. Probably not gona happen, so a design change may be required.

--- return_codes.php
+++ return_codes.php
@@ -13,4 +13,14 @@ $RET_NULL_PARAM = -4;
 $RET_EMAIL_NOT_FOUND = -5;
 $RET_INVALID_PASSWORD = -6;
 
+$status = array(
+    $RET_SUCCESS => "Success",
+    $RET_ERROR => "Error",
+    $RET_EMAIL_NOT_AVAILABLE => "Email not available",
+    $RET_SIGN_UP_FAILED => "Signing up failed",
+    $RET_NULL_PARAM => "Missing information",
+    $RET_EMAIL_NOT_FOUND => "Email not found",
+    $RET_INVALID_PASSWORD => "Invalid password",
+);
+
 ?>
